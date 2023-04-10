afdde70ca4f767aa1f0816ff0d719dc92f279aa7
ngarapdatmanik <ngarapdatmanik@gmail.com>
2023-04-10 13:37:56 +0700

    ubah style

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-const menuToggle = document.querySelector(".toggle")
-const navItem =document.querySelector("nav ul")
+const menuToggle = document.querySelector(".toggle");
+const navItem = document.querySelector("nav ul");
 menuToggle.addEventListener("click", () => {
-    navItem.classList.toggle('slide')
-})
+  navItem.classList.toggle("slide");
+});
+
+const boxes = document.querySelectorAll(".about,.project,.kemampuan,.kontak");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const trigger = (window.innerHeight / 5) * 4;
+
+  for (let i = 0; i < boxes.length; i++) {
+    const boxTop = boxes[i].getBoundingClientRect().top;
+    if (boxTop < trigger) {
+      boxes[i].classList.add("show");
+    } else {
+      boxes[i].classList.remove("show");
+    }
+  }
+}
